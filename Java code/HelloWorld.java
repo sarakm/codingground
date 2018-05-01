632e0cbcba7b8518fef7fcc2872b47639ef39b64
sarakm <sarakm@users.noreply.github.com>
2018-05-01 18:18:05 -0400

    Created by Coding Ground Client

---
+++ Java code/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld{
+    
+    int a= 10;
+    int octal = 0133;
+    int hexa = 0x44;
+    char grade = 'A';
+    String greet = "Hello";
+    
+
+     public static void main(String []args){
+        System.out.println(octal);
+        System.out.println(hexa);
+        System.out.println(grade);
+        System.out.println(greet);
+     }
+}
